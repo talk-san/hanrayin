4b7868e4c623d8120d1958c267e96b7494232e1d
Tariel Hakobyan <tariel.hakobyan03@gmail.com>
2023-12-24 18:41:19 +0400

    First Commit

---
+++ src/main/java/com/tariel/hanrayin/HanrayinApplication.java
@@ -0,0 +1,13 @@
+package com.tariel.hanrayin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanrayinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanrayinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tariel/hanrayin/HanrayinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tariel.hanrayin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanrayinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
